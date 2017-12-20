4ccb4b065eb77a9f5fc030aa060fcd247c974212
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2017-12-20 16:53:56 -0200

    adds new constructors into CurrencyMismatchException

--- src/TeixeiraSoftware/Finance/CurrencyMismatchException.cs
+++ src/TeixeiraSoftware/Finance/CurrencyMismatchException.cs
@@ -17,53 +17,123 @@ namespace TeixeiraSoftware.Finance
         ///     The left operand <see cref="Money" /> instance of the math or comparison
         ///     operation
         /// </summary>
-        public Money LeftOperand { get; }
+        public Money? LeftOperand { get; }
 
         /// <summary>
         ///     The right operand <see cref="Money" /> instance of the math or comparison
         ///     operation
         /// </summary>
-        public Money RightOperand { get; }
+        public Money? RightOperand { get; }
 
         /// <summary>
-        ///     Default constructor. This constructor will NOT save whe <see cref="Money" />
-        ///     instances involved. The message will be the default message:
-        ///     <see cref="DefaultMessage" />
+        ///     This constructor will set the Exception's attributes in the following manner:
+        ///     <para>Message: The default message: <see cref="DefaultMessage" />.</para>
+        ///     <para>LeftOperand: Will be null.</para>
+        ///     <para>RightOperand: Will be null.</para>
+        ///     <para>InnerException: Will be null.</para>
         /// </summary>
-        public CurrencyMismatchException() : this(DefaultMessage) {}
+        public CurrencyMismatchException()
+            : this(DefaultMessage, null, null, null) { }
 
         /// <summary>
-        ///     This constructor will save the <see cref="Money" /> instances involved.
-        ///     The message will be the default message: <see cref="DefaultMessage" />
+        ///     This constructor will set the Exception's attributes in the following manner:
+        ///     <para>Message: According to the given parameter value.</para>
+        ///     <para>LeftOperand: Will be null.</para>
+        ///     <para>RightOperand: Will be null.</para>
+        ///     <para>InnerException: Will be null.</para>
         /// </summary>
-        /// <param name="leftOperand">
-        ///     The <see cref="Money" /> instance on the left side of the math or comparison
-        ///     operation
-        /// </param>
-        /// <param name="rightOperand">
-        ///     The <see cref="Money" /> instance on the right side of the math or comparison
-        ///     operation
-        /// </param>
+        /// <param name="message">The exception message</param>
+        public CurrencyMismatchException(string message)
+            : this(message, null, null, null) { }
+
+        /// <summary>
+        ///     This constructor will set the Exception's attributes in the following manner:
+        ///     <para>Message: The default message: <see cref="DefaultMessage" />.</para>
+        ///     <para>LeftOperand: According to the given parameter value.</para>
+        ///     <para>RightOperand: According to the given parameter value.</para>
+        ///     <para>InnerException: Will be null.</para>
+        /// </summary>
+        /// <param name="leftOperand">The left operand of the operation</param>
+        /// <param name="rightOperand">The right operand of the operation</param>
         public CurrencyMismatchException(Money leftOperand, Money rightOperand)
-            : this()
+            : this(DefaultMessage, leftOperand, rightOperand, null)
+        { }
+
+        /// <summary>
+        ///     This constructor will set the Exception's attributes in the following manner:
+        ///     <para>Message: The default message: <see cref="DefaultMessage" />.</para>
+        ///     <para>LeftOperand: Will be null.</para>
+        ///     <para>RightOperand: Will be null.</para>
+        ///     <para>InnerException: According to the given parameter value.</para>
+        /// </summary>
+        /// <param name="inner">The inner exception</param>
+        public CurrencyMismatchException(Exception inner)
+            : this(DefaultMessage, null, null, inner) { }
+
+        /// <summary>
+        ///     This constructor will set the Exception's attributes in the following manner:
+        ///     <para>Message: According to the given parameter value.</para>
+        ///     <para>LeftOperand: According to the given parameter value.</para>
+        ///     <para>RightOperand: According to the given parameter value.</para>
+        ///     <para>InnerException: Will be null.</para>
+        /// </summary>
+        /// <param name="message">The exception message</param>
+        /// <param name="leftOperand">The left operand of the operation</param>
+        /// <param name="rightOperand">The right operand of the operation</param>
+        public CurrencyMismatchException(
+            string message,
+            Money leftOperand,
+            Money rightOperand
+        ) : this(message, leftOperand, rightOperand, null) { }
+
+        /// <summary>
+        ///     This constructor will set the Exception's attributes in the following manner:
+        ///     <para>Message: According to the given parameter value.</para>
+        ///     <para>LeftOperand: Will be null.</para>
+        ///     <para>RightOperand: Will be null.</para>
+        ///     <para>InnerException: According to the given parameter value.</para>
+        /// </summary>
+        /// <param name="message">The exception message</param>
+        /// <param name="inner">The inner exception</param>
+        public CurrencyMismatchException(string message, Exception inner)
+            : this(message, null, null, inner) { }
+
+        /// <summary>
+        ///     This constructor will set the Exception's attributes in the following manner:
+        ///     <para>Message: The default message: <see cref="DefaultMessage" />.</para>
+        ///     <para>LeftOperand: According to the given parameter value.</para>
+        ///     <para>RightOperand: According to the given parameter value.</para>
+        ///     <para>InnerException: According to the given parameter value.</para>
+        /// </summary>
+        /// <param name="leftOperand">The left operand of the operation</param>
+        /// <param name="rightOperand">The right operand of the operation</param>
+        /// <param name="inner">The inner exception</param>
+        public CurrencyMismatchException(
+            Money leftOperand,
+            Money rightOperand,
+            Exception inner
+        ) : this(DefaultMessage, leftOperand, rightOperand, inner) { }
+
+        /// <summary>
+        ///     This constructor will set the Exception's attributes in the following manner:
+        ///     <para>Message: According to the given parameter value.</para>
+        ///     <para>LeftOperand: According to the given parameter value.</para>
+        ///     <para>RightOperand: According to the given parameter value.</para>
+        ///     <para>InnerException: According to the given parameter value.</para>
+        /// </summary>
+        /// <param name="message">The exception message</param>
+        /// <param name="leftOperand">The left operand of the operation</param>
+        /// <param name="rightOperand">The right operand of the operation</param>
+        /// <param name="inner">The inner exception</param>
+        public CurrencyMismatchException(
+            string message,
+            Money leftOperand,
+            Money rightOperand,
+            Exception inner
+        ) : base(message, inner)
         {
             this.LeftOperand = leftOperand;
             this.RightOperand = rightOperand;
         }
-
-        /// <summary>
-        ///     This constructor will NOT save the <see cref="Money" /> instances involved
-        /// </summary>
-        /// <param name="message">The custom exception message</param>
-        public CurrencyMismatchException(string message)
-            : base(message) { }
-
-        /// <summary>
-        ///     This constructor will NOT save the <see cref="Money" /> instances involved
-        /// </summary>
-        /// <param name="message">The custom exception message</param>
-        /// <param name="inner">The inner <see cref="Exception" /> instance</param>
-        public CurrencyMismatchException(string message, Exception inner)
-            : base(message, inner) { }
     }
 }
